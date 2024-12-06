7773cd1bcccf5c2250e0a741eaa04d30a8201355
KCtops100 <kylecollamer@gmail.com>
2024-12-06 14:59:34 -0500

    changed recipe for mage spell book. changed recipe for archmage spell book.

--- kubejs/server_scripts/expert mode recipes/ars_nouveau.js
+++ kubejs/server_scripts/expert mode recipes/ars_nouveau.js
@@ -5,16 +5,45 @@ ServerEvents.recipes(event => {
     event.recipes.ars_nouveau.imbuement('mysticalagriculture:prosperity_gemstone', 'ars_nouveau:source_gem', 500, [])
     // novice spell book
     event.remove({ id: 'ars_nouveau:novice_spell_book' })
-    event.recipes.botania.runic_altar(
-        'ars_nouveau:novice_spell_book',
+    event.recipes.ars_nouveau.enchanting_apparatus(
         [
-            'occultism:taboo_book',
             Item.of('mysticalagriculture:inferium_sword', '{Damage:0}'),
             Item.of('mysticalagriculture:inferium_pickaxe', '{Damage:0}'),
             Item.of('mysticalagriculture:inferium_shovel', '{Damage:0}'),
             Item.of('mysticalagriculture:inferium_axe', '{Damage:0}')
         ],
-        5000
-    )
-    event.recipes.botania.runic_altar("minecraft:acacia_boat", ["minecraft:acacia_button", "minecraft:acacia_door"], 5000)
+	    'occultism:taboo_book',
+	    'ars_nouveau:novice_spell_book',
+	    1000,
+	    true
+	);
+    // mage's spell book
+    event.remove({ id: 'ars_nouveau:apprentice_spell_book_upgrade' })
+    event.recipes.ars_nouveau.enchanting_apparatus(
+        [
+            'forbidden_arcanus:obsidian_ingot',
+            '3x mysticalagriculture:prudentium_gemstone',
+            '2x botania:quartz_blaze',
+            '2x naturesaura:fortress_finder'
+        ],
+	    'ars_nouveau:novice_spell_book',
+	    'ars_nouveau:apprentice_spell_book',
+	    2000,
+	    true
+	);
+    // archmage spell book
+    event.remove({ id: 'ars_nouveau:archmage_spell_book_upgrade' })
+    event.recipes.ars_nouveau.enchanting_apparatus(
+        [
+            'forbidden_arcanus:dark_nether_star',
+            'ars_nouveau:wilden_tribute',
+            '2x minecraft:emerald',
+            '3x thermal:enderium_ingot',
+            'minecraft:totem_of_undying'
+        ],
+	    'ars_nouveau:apprentice_spell_book',
+	    'ars_nouveau:archmage_spell_book',
+	    4000,
+	    true
+	);
 })
